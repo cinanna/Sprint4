3618e86c06d73f426bfccc2fe541dc50d28e55ae
cinanna <cinnabarworld@gmail.com>
2022-12-30 00:45:02 +0700

    + tests

--- src/test/java/Sprint4Tests/TestsForTaskOne.java
+++ src/test/java/Sprint4Tests/TestsForTaskOne.java
@@ -1,2 +1,102 @@
-package Sprint4Tests;public class TestsForTaskOne {
+package Sprint4Tests;
+
+import org.example.MainPage;
+import org.example.PageObjects.PageObjectsForMainPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.assertEquals;
+
+public class TestsForTaskOne {
+    private WebDriver driver;
+
+    PageObjectsForMainPage pom = new PageObjectsForMainPage();
+
+    @Before
+    public void startUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void checkFirstFAQ() {
+        String expectedTextOne = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage();
+        String result = mainPage.getTextInFAQ(pom.button, pom.textInButton);
+        assertEquals("Не совпадает текст", expectedTextOne, result);
+    }
+
+    @Test
+    public void checkSecondFAQ() {
+        String expectedTextTwo = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage();
+        String result = mainPage.getTextInFAQ(pom.buttonOne, pom.textInButtonOne);
+        assertEquals("Не совпадает текст", expectedTextTwo, result);
+    }
+
+    @Test
+    public void checkThirdFAQ() {
+        String expectedTextThree = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage();
+        String result = mainPage.getTextInFAQ(pom.buttonTwo, pom.textInButtonTwo);
+        assertEquals("Не совпадает текст", expectedTextThree, result);
+    }
+
+    @Test
+    public void checkFourFAQ() {
+        String expectedTextFour = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage();
+        String result = mainPage.getTextInFAQ(pom.buttonThree, pom.textInButtonThree);
+        assertEquals("Не совпадает текст", expectedTextFour, result);
+    }
+
+    @Test
+    public void checkFiveFAQ() {
+        String expectedTextFive = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage();
+        String result = mainPage.getTextInFAQ(pom.buttonFour, pom.textInButtonFour);
+        assertEquals("Не совпадает текст", expectedTextFive, result);
+    }
+
+    @Test
+    public void checkSixFAQ() {
+        String expectedTextSix = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage();
+        String result = mainPage.getTextInFAQ(pom.buttonFive, pom.textInButtonFive);
+        assertEquals("Не совпадает текст", expectedTextSix, result);
+    }
+
+    @Test
+    public void checkSevenFAQ() {
+        String expectedTextSeven = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage();
+        String result = mainPage.getTextInFAQ(pom.buttonSix, pom.textInButtonSix);
+        assertEquals("Не совпадает текст", expectedTextSeven, result);
+    }
+
+    @Test
+    public void checkEightFAQ() {
+        String expectedTextEight = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage();
+        String result = mainPage.getTextInFAQ(pom.buttonSeven, pom.textInButtonSeven);
+        assertEquals("Не совпадает текст", expectedTextEight, result);
+    }
+
+
+    @After
+    public void endTest() {
+        driver.quit();
+    }
 }
--- src/test/java/Sprint4Tests/TestsForTaskTwo.java
+++ src/test/java/Sprint4Tests/TestsForTaskTwo.java
@@ -1,2 +1,76 @@
-package Sprint4Tests;public class TestsForTaskTwo {
+package Sprint4Tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.example.MainPage;
+import org.example.OrderPage;
+import org.example.PostOrder;
+import org.example.RentPage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class TestsForTaskTwo {
+    private final String name;
+    private final String surname;
+    private final String address;
+    private final String phone;
+    private WebDriver driver;
+    public boolean isOrdered;
+
+    public TestsForTaskTwo (String name, String surname, String address, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] testData() {
+        return new Object[][] {
+                { "Луффи", "Мугивара", "Москва", "88005555555"},
+                { "Джорно", "Джованна", "Санкт-Петербург", "88001111111"},
+        };
+    }
+
+
+
+    @Before
+    public void startUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+
+    @Test
+    public void checkOrder() {
+        driver = new ChromeDriver();
+        OrderPage orderPage = new OrderPage(driver);
+        MainPage mainPage = new MainPage(driver);
+        RentPage rentPage = new RentPage(driver);
+        PostOrder postOrder = new PostOrder(driver);
+        mainPage.openMainPage();
+        orderPage.openOrderPage();
+        orderPage.fillOrderPage(name, surname, address, phone);
+
+        rentPage.openRentPage();
+        rentPage.fillRentInfo("05.01.2023", "Без комментариев");
+
+        postOrder.confirmOrder();
+
+        isOrdered = postOrder.isOrderTrue();
+
+        assertTrue("Заказ не оформлен", isOrdered);
+
+    }
+
+    @After
+    public void endTest() {
+        driver.quit();
+    }
 }
